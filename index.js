1ef39d4486c3eaa77c637847f7581a44ad8024a5
asonuc <120679474+hzumre@users.noreply.github.com>
2024-04-07 13:54:37 +0300

    updated.

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
+const express = require('express');
 const http = require('http');
+const mysql = require('mysql');
+
+const app = express();
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
+const dbConnection = mysql.createConnection({
+  host: 'lab-db.crc60yuwczt8.eu-north-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'GSQ2Dar9MMRG7fyullod',
+  database: 'students'
 });
 
-app.get("/students", (req, res) => {
-  dbConnection.query("SELECT * FROM students", (err, results, fields) => {
+dbConnection.connect((err) => {
+  if (err) {
+    console.error('Database connection failed: ', err);
+    return;
+  }
+  console.log('Connected to the database.');
+});
+
+app.get("/student", (req, res) => {
+  dbConnection.query("SELECT * FROM student", (err, results, fields) => {
     if (err) {
       console.log("Database query error: ", err);
+      res.status(500).json({
+        status: "error",
+        message: "An error occurred while fetching student."
+      });
     } else {
       res.status(200).json({
         status: "success",
@@ -20,6 +37,36 @@ app.get("/students", (req, res) => {
   });
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+const bodyParser = require('body-parser');
+
+// JSON gövdelerini işlemek için body-parser kullanımı
+app.use(bodyParser.json());
+
+// POST isteğini işlemek için endpoint
+app.post("/student/add", (req, res) => {
+  const ad = req.body.ad;
+  const surname = req.body.surname;
+
+  const sql = "INSERT INTO student (ad, surname) VALUES (?, ?)";
+  dbConnection.query(sql, [ad, surname], (err, results, fields) => {
+    if (err) {
+      console.log("Database query error: ", err);
+      res.status(500).json({
+        status: "error",
+        message: "An error occurred while adding the student."
+      });
+    } else {
+      res.status(200).json({
+        status: "success",
+        message: "Student added successfully."
+      });
+    }
+  });
 });
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server running on localhost:${port}/`);
+});
+
+
