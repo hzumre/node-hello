4946dc397a5696089cd84b2f6c34213109c383f6
Hatice Zümre Sonuç <120679474+hzumre@users.noreply.github.com>
2024-04-07 12:32:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const server = http.createServer((req, res) => {
   res.end(msg);
 });
 
+app.get("/students", (req, res) => {
+  dbConnection.query("SELECT * FROM students", (err, results, fields) => {
+    if (err) {
+      console.log("Database query error: ", err);
+    } else {
+      res.status(200).json({
+        status: "success",
+        data: results,
+      });
+    }
+  });
+});
+
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
